ea5b4fee5e6633698bb906e7874b591b500df760
tushar2407 <tushar.mohan2001@gmail.com>
2021-04-06 04:14:21 +0530

    db credentials of heroku

--- modules/db.py
+++ modules/db.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 def connect_to_db():
     conn = psycopg2.connect(
-        database = "DBMS",
-        user = "postgres",
-        password = os.environ.get('PASSWORD'),
-        host = "localhost"
+        database = "dafonabp5estsj",
+        user = "krpnttxzepxgmc",
+        password = "36bbd13385dc3c89abdfe15879f1a0bfbc029d0ee9d89e49702d9e75f3eda437",
+        host = "ec2-52-1-115-6.compute-1.amazonaws.com"
     )
     conn.autocommit = True
     return conn.cursor()
